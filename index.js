3996cdfb5838635e39d52a6f1b3dd288e3bbe87e
Aadil Hassan <68943818+Aadilhassan@users.noreply.github.com>
2022-11-29 07:22:10 +0000

    fetching all file names

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ import fs, { createReadStream } from "fs"
 import { S3Client, PutObjectCommand, CompleteMultipartUploadCommand } from "@aws-sdk/client-s3";
 import AWS from 'aws-sdk'
 import { s3Client } from "./s3Client.js";
-import extractZip from "extract-zip"
-import { setTimeout } from "timers/promises";
-import { on } from "events";
+// import extractZip from "extract-zip"
+// import { setTimeout } from "timers/promises";
+// import { on } from "events";
 import stream from "stream";
 import { unlink } from "fs";
 
 
 const s3 = new AWS.S3()
 
-
+const arr = []
 
 
 async function main(){
@@ -26,39 +26,75 @@ async function main(){
 
 async function readZipFile(){
 
-  fs.createReadStream('./uploadWgene.zip')
-  .pipe(unzipper.Parse())
-  .pipe(stream.Transform({
-    objectMode: true,
-    transform: function(entry,e,cb) {
-      const fileName = entry.path;
-      const na = fileName.split('/pub2/')[1]
-      const type = entry.type; // 'Directory' or 'File'
-      const size = entry.vars.uncompressedSize; // There is also compressedSize;
-      if (type === 'File') {
-        console.log(fileName)
-        async function up(){
-          entry.pipe(fs.createWriteStream('output/'+na))
-          .on('finish', cb)
-          .on('finish', async function(){
-          await  uploadFile('output/'+na)
-          })
+//   fs.createReadStream('./uploadWgene.zip')
+//   .pipe(unzipper.Parse())
+//   .pipe(stream.Transform({
+//     objectMode: true,
+//     transform: function(entry,e,cb) {
+//       const fileName = entry.path;
+//       const na = fileName.split('/pub2/')[1]
+//       const type = entry.type; // 'Directory' or 'File'
+//       const size = entry.vars.uncompressedSize; // There is also compressedSize;
+//       if (type === 'File') {
+//         console.log(fileName)
+//         async function up(){
+//             arr.push(entry.path)
+//             console.log(arr)
+//         //   entry.pipe(fs.createWriteStream('output/'+na))
+//         //   .on('finish', cb)
+//         //   .on('finish', async function(){
+//         //   await  uploadFile('output/'+na)
+//         // arr.push(na)
+//         // console.log(arr)
+//         //   })
           
-        }
-        up()
+//         }
+//         up()
       
 
             
           
 
-      } else {
-        entry.autodrain();
-        cb();
-      }
-    }
-  }
-))
+//       } else {
+//         entry.autodrain();
+//         cb();
+//       }
+//     }
+//   }
+// ))
   
+
+const uu = await unzipper.Open.file('./uploadWgene.zip')
+const file = uu.files
+console.log(file.length)
+fs.appendFileSync('./ik.js', 'const arr = [')
+for(let i = 0; i < file.length; i++){
+  const na = file[i].path.split('/pub2/')[1]
+  console.log(file[i].path)
+  if(file[i].type === 'File'){
+fs.appendFileSync('./ik.js',`" ${file[i].path}",`)
+} else{
+
+}
+}
+fs.appendFileSync('./ik.js', ']')
+
+// fs.createReadStream('./uploadWgene.zip')
+//     .pipe(unzipper.Parse())
+//     .on('entry', async function (entry) {
+//         const fileName = entry.path;
+//         const na = fileName.split('/pub2/')[1]
+//         const type = entry.type; // 'Directory' or 'File'
+//         const size = entry.vars.uncompressedSize; // There is also compressedSize;
+//         if (type === 'File') {
+//            console.log(fileName)
+//         } else {
+//             console.log(fileName)
+//             entry.autodrain();
+//         }   
+//     })
+
+
 
 
 
